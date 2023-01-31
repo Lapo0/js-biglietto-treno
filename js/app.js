9e14feacaa96545f4c6be516cd69404e0fb8de58
Lapo0 <lapo.gini.01@gmail.com>
2023-01-31 15:34:57 +0100

    FEAT app.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,32 @@
 // Quanti chilometri devi percorre
-const chilometriDaPercorrere = parseInt( prompt('Quanti chilometri devi percorrere:'))
+const chilometriDaPercorrere = parseFloat( prompt('Quanti chilometri devi percorrere?'))
 
 console.log ('Devi percorrere:', chilometriDaPercorrere, 'km')
 
 // Qual è la tua età
+const eta = parseInt(prompt ('Quanti anni hai?'))
 
+console.log ('Hai:', eta, 'anni')
 
-// Prezzo totale
-// Sconto per i minorenni
-// Sconto per gli over 65
+// Prezzo al chilometro
+const prezzoAlChilometro = 0.21
+
+console.log (prezzoAlChilometro, '€/km')
+
+// Prezzo totale per il viaggio
+const prezzoTotale = chilometriDaPercorrere * prezzoAlChilometro
+
+console.log(prezzoTotale.toFixed(2))
+
+    // Sconto minorenne
+    if (eta < 18) {
+        const prezzoMinorenni = ((prezzoTotale / 100) * 80)
+
+        console.log (prezzoMinorenni.toFixed(2))
+
+    // Sconto Over 65    
+    } else if (eta > 65) {
+        const prezzoOver65 = ((prezzoTotale / 100) * 60)
+
+        console.log (prezzoOver65.toFixed(2))
+    }
