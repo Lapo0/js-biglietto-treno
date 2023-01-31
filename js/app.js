d1d8c40b474c4f52d7ad520d48e48fa576ec2beb
Lapo0 <lapo.gini.01@gmail.com>
2023-01-31 15:05:31 +0100

    START to cry, goodbye Riccardo, we will miss you <3

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// Quanti chilometri devi percorre
+const chilometriDaPercorrere = parseInt( prompt('Quanti chilometri devi percorrere:'))
+
+console.log ('Devi percorrere:', chilometriDaPercorrere, 'km')
+
+// Qual è la tua età
+
+
+// Prezzo totale
+// Sconto per i minorenni
+// Sconto per gli over 65
